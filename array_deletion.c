576f80ba03e4316a97201b335a42e2efbf7be774
CoderTahmid <tahmid.ibne@gmail.com>
2025-09-27 10:49:24 +0600

    array deletion added

---
+++ array_deletion.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the array size : ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the array : ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int num;
+    printf("Enter the number you want to delete : ");
+    scanf("%d", &num);
+
+    int idx = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == num) {
+            idx = i;
+            break;
+        }
+    }
+
+    if (idx > -1) {
+        printf("Before delete : ");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+
+        for (int i = idx; i < n - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        printf("After delete : ");
+        for (int i = 0; i < n - 1; i++) {
+            printf("%d ", arr[i]);
+        }
+    } else {
+        printf("The number does not exists\n");
+    }
+}
